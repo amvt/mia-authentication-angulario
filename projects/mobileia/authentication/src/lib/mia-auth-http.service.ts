6393c61a791afdb7ee6a01dcbaa22fed6ee434aa
Matias Camiletti <matias.camiletti@gmail.com>
2020-05-19 19:13:21 -0300

    Agregado promesas para simplicar las llamadas.

--- projects/mobileia/authentication/src/lib/mia-auth-http.service.ts
+++ projects/mobileia/authentication/src/lib/mia-auth-http.service.ts
@@ -14,13 +14,41 @@ export class MiaAuthHttpService extends MiaHttpService {
         super(http);
         this._httpReference = http;
         this._authServiceReference = authService;
-     }
+    }
+
+    public postAuthObjectPro(url: string, params: any): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            this.postAuthObject(url, params).toPromise().then(result => {
+              if (result.success) {
+                resolve(result.response);
+              } else {
+                reject(result.error);
+              }
+            }).catch(error => {
+              reject(error);
+            });
+        });
+    }
 
     public postAuthObject(url: string, params: any): Observable<ApiResponse<any>> {
         return this._authServiceReference.getAccessToken().pipe(switchMap(token => {
             params.access_token = token;
             return this.postObject(url, params);
         }));
+    }
+
+    public postAuthArrayPro(url: string, params: any): Promise<[any]> {
+        return new Promise<any>((resolve, reject) => {
+            this.postAuthArray(url, params).toPromise().then(result => {
+              if (result.success) {
+                resolve(result.response);
+              } else {
+                reject(result.error);
+              }
+            }).catch(error => {
+              reject(error);
+            });
+        });
     }
 
     public postAuthArray(url: string, params: any): Observable<ApiResponse<[any]>> {
